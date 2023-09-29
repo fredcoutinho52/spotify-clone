e66bd34628d46af4486cd97e366e011d3eb30907
Fred Coutinho <fredcoutinho52@gmail.com>
2023-09-29 15:40:40 -0300

    formatando o código

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
-import Routes from './src/routes'
+import Routes from "./src/routes";
 
 export default function App() {
   return (
     <Routes/>
-  )
+  );
 }
